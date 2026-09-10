ef8d80903334c780f591ef1ca475b46e97a0c207
ericksitocm <ericcortes48@aragon.unam.mx>
2026-09-09 21:25:41 -0600

    Se agrega Tarea 3}

---
+++ T3JSPC -Cortes Mendoza Eric/frutasfr.js
@@ -0,0 +1,16 @@
+const carritofrutas = []
+
+//Programa que simula un carro de compras con un prompt que le pregunta al usuario si quiere comprar una fruta
+var fruta;
+const frutas = [];
+while (true){
+      if(confirm("¿Quieres agregar otra fruta?")){
+        fruta = prompt("¿Qué fruta?")
+        frutas.push(fruta)
+      } else {
+        break;
+      }  
+}
+frutas.forEach((fruto,index,frutas) =>{
+    console.log(fruto + " " + index);
+})
